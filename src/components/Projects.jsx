5c57223fd225d3ac9dabd5bc3b77f43850095f4e
frshaad <farshad.hatami95@gmail.com>
2022-11-28 09:17:22 +0330

    project section add blured glass

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ export const Projects = () => {
   return (
     <section className='h-screen w-screen snap-start bg-gray-50 dark:bg-black'>
       <article
-        className='flex h-full w-full flex-col justify-between bg-cover bg-center bg-no-repeat py-14 px-10 brightness-95 saturate-[0.8] dark:brightness-90 lg:px-44 lg:py-36'
+        className='flex h-full w-full flex-col justify-between bg-cover bg-center bg-no-repeat py-14 px-10 brightness-95 saturate-[0.9] dark:brightness-90 lg:px-44 lg:py-36'
         style={{ backgroundImage: `url(${projects[activeProject].image})` }}
       >
         <p>
@@ -36,11 +36,11 @@ export const Projects = () => {
         </p>
 
         <div className='flex items-center justify-between '>
-          <div className='flex flex-col gap-3 lg:gap-5'>
-            <p className='text-3xl font-bold capitalize text-gray-900 lg:text-5xl'>
+          <div className='flex flex-col gap-3 rounded-lg bg-[rgba(255,255,255,0.1)] p-8 shadow-lg backdrop-blur-lg hover:shadow-2xl lg:gap-5 hover:lg:scale-105'>
+            <p className='text-3xl font-bold capitalize text-gray-900 lg:text-7xl'>
               {projects[activeProject].name}
             </p>
-            <p className='mb-10 text-lg capitalize text-gray-800 lg:text-xl'>
+            <p className='mb-10 text-lg capitalize text-gray-800 lg:text-2xl'>
               {projects[activeProject].location}
             </p>
             <button className='flex items-center gap-2 self-baseline rounded-full bg-gray-50 px-8 py-3 text-lg font-medium capitalize text-gray-700 shadow-lg lg:hover:gap-4'>
@@ -48,11 +48,11 @@ export const Projects = () => {
             </button>
           </div>
           <p>
-            <span className='font-sliderNum text-5xl font-semibold'>
+            <span className='font-sliderNum text-5xl font-semibold lg:text-7xl'>
               {projects[activeProject].id}
             </span>
-            <span className='text-5xl font-normal'>/</span>
-            <span className='font-sliderNum text-3xl font-normal'>
+            <span className='text-5xl font-normal lg:text-7xl'>/</span>
+            <span className='font-sliderNum text-3xl font-normal lg:text-5xl'>
               {projects.length < 9 ? '0' : ''}
               {projects.length}
             </span>
