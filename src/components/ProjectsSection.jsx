43efa60bd27ac78cee6e52fcec9d7c44360cbfbe
frshaad <farshad.hatami95@gmail.com>
2023-01-19 22:00:26 +0330

    ProjectsSection Responsive done

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -26,9 +26,9 @@ export const ProjectsSection = () => {
   };
 
   return (
-    <section className='relative h-screen w-screen bg-gray-50 dark:bg-black'>
-      <div className='m-auto flex h-full w-11/12 items-center justify-center py-12'>
-        <div>
+    <section className='relative h-max w-screen bg-gray-50 dark:bg-black sm:h-screen'>
+      <div className='m-auto flex h-full w-11/12 flex-col items-center justify-center py-12 lg:flex-row'>
+        <div className='mb-2'>
           <img
             src={projects[activeProject].image}
             className='h-auto w-full rounded-xl drop-shadow-2xl'
@@ -36,30 +36,30 @@ export const ProjectsSection = () => {
           />
         </div>
 
-        <div className='relative right-24 mx-auto flex h-fit w-full flex-col rounded-xl bg-[rgb(255,255,255,0.5)] p-10 capitalize shadow-2xl drop-shadow-2xl backdrop-blur-md  dark:bg-[rgb(0,0,0,0.5)]  md:w-2/3 lg:w-1/2 xl:w-5/12 2xl:w-3/12'>
+        <div className='relative mx-auto flex h-fit w-full flex-col rounded-xl bg-[rgb(255,255,255,0.5)] px-10 py-4 capitalize shadow-2xl drop-shadow-2xl backdrop-blur-md md:w-2/3  lg:right-24  lg:w-1/2 lg:p-10 xl:w-5/12 2xl:w-3/12'>
           <ProjectsTitle />
           <div className='py-2'>
             <h6>Project</h6>
-            <p className=' text-lg font-bold text-gray-700 dark:text-gray-200'>
+            <p className=' text-lg font-bold text-gray-700 '>
               {projects[activeProject].name}
             </p>
           </div>
           <div className='py-2'>
             <h6>Location</h6>
-            <p className=' text-lg font-bold text-gray-700 dark:text-gray-200'>
+            <p className=' text-lg font-bold text-gray-700 '>
               {projects[activeProject].location}
             </p>
           </div>
           <div className='py-2'>
             <h6>Year</h6>
-            <p className=' text-lg font-bold text-gray-700 dark:text-gray-200'>
+            <p className=' text-lg font-bold text-gray-700 '>
               {projects[activeProject].year}
             </p>
           </div>
           <div className='mb-8 py-2'>
             <p>
               <span>as </span>
-              <span className='text-lg font-bold text-gray-700 dark:text-gray-200'>
+              <span className='text-lg font-bold text-gray-700 '>
                 {projects[activeProject].role}
               </span>
             </p>
@@ -67,7 +67,7 @@ export const ProjectsSection = () => {
           <div className='mb-4 flex '>
             <button
               onClick={toggleModal}
-              className='flex items-center font-semibold capitalize text-gray-700 dark:text-gray-200'
+              className='flex items-center font-semibold capitalize text-gray-700'
             >
               <MdOutlineLocalSee className='mr-1 text-sm md:text-lg' />
               see project
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from 'react'
-import { ShowcaseContent } from './ShowcaseContent'
-import { ShowcaseSlider } from './ShowcaseSlider'
+import { useEffect, useState } from "react";
+import { ShowcaseContent } from "./ShowcaseContent";
+import { ShowcaseSlider } from "./ShowcaseSlider";
 
 export const Showcase = () => {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false)
-    }, 5000)
-  }, [])
+      setIsLoading(false);
+    }, 5000);
+  }, []);
 
   if (isLoading) {
-    return <h1>loading</h1>
+    return <h1>loading</h1>;
   }
 
   return (
-    <section className='flex flex-col-reverse  bg-gray-50 transition duration-300 dark:bg-black xl:max-h-screen xl:flex-row '>
+    <section className='flex flex-col-reverse bg-gray-50 transition duration-300 dark:bg-black lg:max-h-screen lg:flex-row '>
       <ShowcaseSlider />
       <ShowcaseContent />
     </section>
-  )
-}
+  );
+};
