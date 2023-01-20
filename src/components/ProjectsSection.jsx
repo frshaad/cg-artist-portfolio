0c220c020053f4657a1ce98ce2ff5c9f15d644b5
frshaad <farshad.hatami95@gmail.com>
2023-01-20 15:44:07 +0330

    Bugs Fixed

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -26,9 +26,9 @@ export const ProjectsSection = () => {
   };
 
   return (
-    <section className='relative mb-24 w-screen bg-gray-50 dark:bg-black md:mb-0 md:h-screen lg:flex lg:items-center'>
-      <div className='m-auto flex w-11/12 flex-col items-center justify-center lg:flex-row'>
-        <div className='mb-2 lg:mb-0'>
+    <section className='relative mb-32 w-screen bg-gray-50 dark:bg-black lg:flex lg:items-center'>
+      <div className='m-auto flex flex-col items-center justify-center p-4 md:p-10 lg:flex-row'>
+        <div className='mb-2 max-w-6xl lg:mb-0'>
           <img
             src={projects[activeProject].image}
             className='h-auto w-full rounded-xl drop-shadow-2xl'
@@ -36,7 +36,7 @@ export const ProjectsSection = () => {
           />
         </div>
 
-        <div className='relative mx-auto flex h-fit w-full flex-col rounded-xl bg-[rgb(255,255,255,0.5)] px-10 py-4 capitalize shadow-2xl drop-shadow-2xl backdrop-blur-md md:-top-16 md:w-2/3 lg:top-0  lg:right-24  lg:w-1/2 lg:p-10 xl:w-5/12 2xl:w-3/12'>
+        <div className='relative mx-auto flex h-fit w-full flex-col rounded-xl bg-[rgb(255,255,255,0.5)] px-10 py-4 capitalize shadow-2xl drop-shadow-2xl backdrop-blur-md md:-top-16 md:w-2/3 lg:top-0  lg:right-24  lg:max-w-sm lg:p-10'>
           <ProjectsTitle />
           <div className='py-2'>
             <h6>Project</h6>
