7c029b3035854f8217b7aefd029dc7d1e27669b6
frshaad <farshad.hatami95@gmail.com>
2023-01-21 16:40:20 +0330

    emailjs setup done

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,24 +3,43 @@ import { useRef } from "react";
 import { BiMailSend } from "react-icons/bi";
 
 export const ContactForm = () => {
+  const emailJsServiceID = import.meta.env.VITE_EMAILJS_SERVICEID;
+  const emailJsTemplateID = import.meta.env.VITE_EMAILJS_TEMPLATEID;
+  const emailJsPublicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
   const form = useRef();
+  const submitBtn = document.getElementById("submit-btn");
+  // const userName = document.getElementById("user-name");
+  // const userEmail = document.getElementById("user-email");
+  // const userMessage = document.getElementById("user-message");
 
   const sendEmail = (e) => {
     e.preventDefault();
+    submitBtn.value = "Sending...";
 
     emailjs
       .sendForm(
-        "mahdi_bahmani",
-        "template_y8oz8gf",
+        emailJsServiceID,
+        emailJsTemplateID,
         form.current,
-        "1H21uTkhIHaS-KyqI"
+        emailJsPublicKey
       )
       .then(
         (result) => {
-          console.log(result.text);
+          submitBtn.classList.add("text-green-700");
+          submitBtn.value = "Sent!";
+          setTimeout(() => {
+            submitBtn.value = "Send";
+            submitBtn.classList.remove("text-green-700");
+          }, 10000);
         },
         (error) => {
-          console.log(error.text);
+          submitBtn.classList.add("text-red-700");
+          submitBtn.value = "Not Sent!";
+          setTimeout(() => {
+            submitBtn.value = "Send";
+            submitBtn.classList.remove("text-green-700");
+          }, 10000);
         }
       );
   };
@@ -32,6 +51,7 @@ export const ContactForm = () => {
       className='grid grid-cols-1 justify-items-center gap-5'
     >
       <input
+        id='user-name'
         type='text'
         name='user_name'
         placeholder="What's your name?"
@@ -40,6 +60,7 @@ export const ContactForm = () => {
       />
 
       <input
+        id='user-email'
         type='email'
         name='user_email'
         placeholder='Enter your email'
@@ -48,6 +69,7 @@ export const ContactForm = () => {
       />
 
       <textarea
+        id='user-meassage'
         name='message'
         placeholder='Write down the message...'
         required
@@ -55,6 +77,7 @@ export const ContactForm = () => {
         className='contact-input'
       />
       <input
+        id='submit-btn'
         type='submit'
         value='Send'
         className='mx-auto w-1/3 cursor-pointer pt-3 text-xl font-medium text-cyan-800 dark:text-cyan-500'
