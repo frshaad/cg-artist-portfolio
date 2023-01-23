0509c2ddaa84ca2026d0e694809fe75eb81f0501
frshaad <farshad.hatami95@gmail.com>
2023-01-23 20:42:43 +0330

    Extra files removed!

--- src/data/services.js
+++
@@ -1,5 +0,0 @@
-export const services = [
-  '3D Architectural and Constructions Visualizations',
-  'Rendering Arch-Viz, Products and Concepts',
-  'Architectural Design: Interior, Exterior and Landscape',
-]
