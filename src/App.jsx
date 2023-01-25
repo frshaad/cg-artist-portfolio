0d994dec28f29db385384d9cf449598c47cc8c31
Farshad <farshad.hatami95@gmail.com>
2023-01-25 17:34:01 +0330

    Minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from "react";
-// import Fade from "react-reveal/Fade";
-// import { AnimatePresence, motion } from "framer-motion";
-import HashLoader from "react-spinners/HashLoader";
 import { DarkModeProvider } from "./contexts/DarkMode";
 import { Home } from "./pages/Home";
 import "./styles/loading-screen.scss";
@@ -19,9 +16,6 @@ export const App = () => {
   return (
     <DarkModeProvider>
       {loading ? (
-        // <div id='loading-container'>
-        //   <HashLoader color='#36d7b7' loading size={120} speedMultiplier={1} />
-        // </div>
         <div class='patterns fade-loading'>
           <svg width='100%' height='100%'>
             <defs>
@@ -33,12 +27,13 @@ export const App = () => {
                 height='100'
                 patternUnits='userSpaceOnUse'
               >
-                {/* <circle fill='rgb(14, 116, 144)' cx='25' cy='25' r='3'></circle> */}
+                {/* <circle
+                  fill='rgba(14, 116, 144, 0.2)'
+                  cx='25'
+                  cy='25'
+                  r='3'
+                ></circle> */}
               </pattern>
-              <style>
-                @import url("https://fonts.googleapis.com/css?
-                family=Lora:400,400i,700,700i");
-              </style>
             </defs>
 
             <rect
