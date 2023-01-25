3f272e9e805547af31ac34d52ecd8fb71130a590
Farshad <farshad.hatami95@gmail.com>
2023-01-25 17:29:31 +0330

    Add more animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import HashLoader from "react-spinners/HashLoader";
 import { DarkModeProvider } from "./contexts/DarkMode";
 import { Home } from "./pages/Home";
+import "./styles/loading-screen.scss";
 
 export const App = () => {
   const [loading, setLoading] = useState(false);
@@ -18,8 +19,47 @@ export const App = () => {
   return (
     <DarkModeProvider>
       {loading ? (
-        <div id='loading-container'>
-          <HashLoader color='#36d7b7' loading size={120} speedMultiplier={1} />
+        // <div id='loading-container'>
+        //   <HashLoader color='#36d7b7' loading size={120} speedMultiplier={1} />
+        // </div>
+        <div class='patterns fade-loading'>
+          <svg width='100%' height='100%'>
+            <defs>
+              <pattern
+                id='polka-dots'
+                x='0'
+                y='0'
+                width='100'
+                height='100'
+                patternUnits='userSpaceOnUse'
+              >
+                {/* <circle fill='rgb(14, 116, 144)' cx='25' cy='25' r='3'></circle> */}
+              </pattern>
+              <style>
+                @import url("https://fonts.googleapis.com/css?
+                family=Lora:400,400i,700,700i");
+              </style>
+            </defs>
+
+            <rect
+              x='0'
+              y='0'
+              width='100%'
+              height='100%'
+              fill='url(#polka-dots)'
+            >
+              {" "}
+            </rect>
+
+            <text
+              x='50%'
+              y='50%'
+              className='text-2xl font-display uppercase md:text-6xl xl:text-9xl'
+              text-anchor='middle'
+            >
+              Mahdi Bahmani
+            </text>
+          </svg>
         </div>
       ) : (
         <Home />
--- src/components/ShowcaseInfo.jsx
+++ src/components/ShowcaseInfo.jsx
@@ -1,20 +1,41 @@
 import { Tb3DCubeSphere } from "react-icons/tb";
+import "../styles/intro-title.scss";
+import "../styles/word-animation.scss";
 
 export const ShowcaseInfo = () => {
   return (
     <div className='relative mx-auto flex w-full flex-col'>
-      <p className='select-none text-center text-lg font-light text-gray-700 dark:text-gray-300 md:text-xl'>
+      {/* <p className='select-none text-center text-lg font-light text-gray-700 dark:text-gray-300 md:text-xl'>
         Hi there! <span className='font-medium'>I am Mahdi Bahmani</span>
+      </p> */}
+
+      <p className='select-none text-center text-lg font-light text-gray-700 dark:text-gray-300 md:text-xl'>
+        Hi there!{" "}
+        <span className='font-normal inline-block'>
+          I am{" "}
+          <section class='animation font-semibold inline-block'>
+            <div class='first'>
+              <p>Mahdi Bahmani</p>
+            </div>
+            <div class='second'>
+              <p>an Architect</p>
+            </div>
+            <div class='third'>
+              <p>a CG Artist</p>
+            </div>
+          </section>
+        </span>
       </p>
+
       <div className='mt-10 flex flex-col items-center justify-center p-3 md:gap-3 md:p-0'>
-        <p className='text-right relative mb-3 flex select-none items-center font-display text-5xl capitalize text-gray-900 dark:text-gray-100 md:-left-24 md:mb-0 md:text-8xl xl:-left-32 xl:text-9xl'>
+        <p className='word relative mb-3 flex select-none items-center font-display text-5xl capitalize text-gray-900 dark:text-gray-100 md:-left-24 md:mb-0 md:text-8xl xl:-left-32 xl:text-9xl'>
           extrude{" "}
           <Tb3DCubeSphere className='h-16 w-16 animate-pulse stroke-gray-900 stroke-1 transition duration-500 hover:rotate-180 dark:stroke-gray-100 md:h-24 md:w-24 md:animate-none xl:h-28 xl:w-28' />
         </p>
-        <p className='text-left relative mb-3 select-none font-display text-5xl font-semibold capitalize text-gray-900 dark:text-gray-100 md:left-10 md:mb-0 md:text-8xl xl:left-20 xl:text-9xl'>
+        <p className='word relative mb-3 select-none font-display text-5xl font-semibold capitalize text-gray-900 dark:text-gray-100 md:left-10 md:mb-0 md:text-8xl xl:left-20 xl:text-9xl'>
           architecture
         </p>
-        <p className='text-right relative mb-3 select-none font-display text-5xl capitalize text-gray-900 dark:text-gray-100 md:-left-24 md:mb-0 md:text-8xl xl:text-9xl'>
+        <p className='word relative mb-3 select-none font-display text-5xl capitalize text-gray-900 dark:text-gray-100 md:-left-24 md:mb-0 md:text-8xl xl:text-9xl'>
           concepts
         </p>
       </div>
